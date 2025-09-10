faad584d61735439cc4fd69bb540afff7f19fc59
Alexey <idolexey@yandex.ru>
2025-09-10 19:44:37 +0500

    Create FastAPI app

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.head("/")
+def read_root(): ...
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
