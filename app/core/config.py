c98c50583a451a9cdc8676af86e62c6cc9b17408
holodnyisiemens <idolexey@yandex.ru>
2025-11-30 21:38:06 +0500

    chore: add config.py for app settings

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: SecretStr
+    DB_NAME: str
+
+    @property
+    def database_url_asyncpg(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
