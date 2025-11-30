e0d981e8daed221e9841d84ab6bcba51131df624
holodnyisiemens <idolexey@yandex.ru>
2025-12-01 01:35:09 +0500

    Add sync engine

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,13 @@ class Settings(BaseSettings):
             f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}@"
             f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
+    
+    @property
+    def database_url_psycopg(self) -> str:
+        return (
+            f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
 
     model_config = SettingsConfigDict(env_file=".env")
 
