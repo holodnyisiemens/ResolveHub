0034136015e8909171542d554015f6749b35246f
holodnyisiemens <idolexey@yandex.ru>
2025-12-02 00:48:06 +0500

    Add unique constraint to employee email

--- app/models/employee.py
+++ app/models/employee.py
@@ -9,5 +9,5 @@ class Employee(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
