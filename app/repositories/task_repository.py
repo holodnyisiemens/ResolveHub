a33b3cdbf6a1848d9b0a43c902c4f5828146b2d3
holodnyisiemens <idolexey@yandex.ru>
2025-12-01 17:02:00 +0500

    Add CRUD for task repo

---
+++ app/repositories/task_repository.py
@@ -0,0 +1,46 @@
+from typing import Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.task import Task
+from app.schemas.task import TaskAddDTO, TaskUpdateDTO
+
+
+class TaskRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_id(self, task_id: int) -> Optional[Task]:
+        return await self.session.get(Task, task_id)
+
+    async def create(self, task_data: TaskAddDTO) -> Task:
+        task = Task(**task_data.model_dump())
+        self.session.add(task)
+        
+        await self.session.flush()
+        await self.session.refresh(task)
+        
+        return task
+
+    async def delete(self, task_id: int) -> bool:
+        task = await self.session.get(Task, task_id)
+        if not task:
+            return False
+        
+        await self.session.delete(task)
+        await self.session.flush()
+        return True
+
+    async def update(self, task_id: int, task_data: TaskUpdateDTO) -> Optional[Task]:
+        task = await self.session.get(Task, task_id)
+        if not task:
+            return None
+
+        update_data = task_data.model_dump(exclude_unset=True)
+        for field, value in update_data.items():
+            setattr(task, field, value)
+
+        await self.session.flush()
+        await self.session.refresh(task)
+
+        return task
