7acb4097febcb23fc134b80ccd2b530e5b250368
holodnyisiemens <idolexey@yandex.ru>
2025-12-03 04:04:59 +0500

    Edit provide_task_service: add employee_repository dependency

--- app/api/deps.py
+++ app/api/deps.py
@@ -15,18 +15,27 @@ async def provide_db_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_factory() as session:
         yield session
 
-async def provide_employee_repository(db_session: AsyncSession = Depends(provide_db_session)) -> EmployeeRepository:
+async def provide_employee_repository(
+    db_session: AsyncSession = Depends(provide_db_session),
+) -> EmployeeRepository:
     """Провайдер репозитория сотрудников"""
     return EmployeeRepository(db_session)
 
-async def provide_employee_service(employee_repository: EmployeeRepository = Depends(provide_employee_repository)) -> EmployeeService:
+async def provide_employee_service(
+    employee_repository: EmployeeRepository = Depends(provide_employee_repository),
+) -> EmployeeService:
     """Провайдер сервиса сотрудников"""
     return EmployeeService(employee_repository)
 
-async def provide_task_repository(db_session: AsyncSession = Depends(provide_db_session)) -> TaskRepository:
+async def provide_task_repository(
+    db_session: AsyncSession = Depends(provide_db_session),
+) -> TaskRepository:
     """Провайдер репозитория задач"""
     return TaskRepository(db_session)
 
-async def provide_task_service(task_repository: TaskRepository = Depends(provide_task_repository)) -> TaskService:
+async def provide_task_service(
+    task_repository: TaskRepository = Depends(provide_task_repository),
+    employee_repository: EmployeeRepository = Depends(provide_employee_repository),
+) -> TaskService:
     """Провайдер сервиса задач"""
-    return TaskService(task_repository)
+    return TaskService(task_repository, employee_repository)
