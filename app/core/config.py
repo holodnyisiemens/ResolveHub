9864437ed0a0369688b6aa013959075285903611
LAPTOP-I8C4HI8P\Александр <LAPTOP-I8C4HI8P\>
2025-12-02 11:51:46 +0500

    Исправил файл config

--- app/core/config.py
+++ app/core/config.py
@@ -5,12 +5,26 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
-    DB_PASSWORD: str
+    DB_PASSWORD: SecretStr
     DB_NAME: str
 
     database_url_sync: str
     database_url_async: str
 
+    @property
+    def database_url_asyncpg(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+    
+    @property
+    def database_url_psycopg(self) -> str:
+        return (
+            f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
 
+    
 settings = Settings()
