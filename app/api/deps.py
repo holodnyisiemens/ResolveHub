f0bc4b35c88cfed21191cbc9bbb60f30b4c16eeb
holodnyisiemens <idolexey@yandex.ru>
2025-12-03 03:22:35 +0500

    Add tasks dependencies

--- app/api/deps.py
+++ app/api/deps.py
@@ -5,7 +5,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import async_session_factory
 from app.repositories.employee_repository import EmployeeRepository
+from app.repositories.task_repository import TaskRepository
 from app.services.employee_service import EmployeeService
+from app.services.task_service import TaskService
 
 
 async def provide_db_session() -> AsyncGenerator[AsyncSession, None]:
@@ -20,3 +22,11 @@ async def provide_employee_repository(db_session: AsyncSession = Depends(provide
 async def provide_employee_service(employee_repository: EmployeeRepository = Depends(provide_employee_repository)) -> EmployeeService:
     """Провайдер сервиса сотрудников"""
     return EmployeeService(employee_repository)
+
+async def provide_task_repository(db_session: AsyncSession = Depends(provide_db_session)) -> TaskRepository:
+    """Провайдер репозитория задач"""
+    return TaskRepository(db_session)
+
+async def provide_task_service(task_repository: TaskRepository = Depends(provide_task_repository)) -> TaskService:
+    """Провайдер сервиса задач"""
+    return TaskService(task_repository)
