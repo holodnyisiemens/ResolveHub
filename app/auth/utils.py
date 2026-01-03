dee8c7c09da3aab50e6294fdd155571a7e61c311
holodnyisiemens <idolexey@yandex.ru>
2026-01-03 17:59:58 +0500

    Add iat and exp to encode_jwt

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, timezone
+
 import bcrypt
 import jwt
 
@@ -8,9 +10,23 @@ def encode_jwt(
     payload: dict,
     private_key: str = settings.auth_jwt.private_key_path.read_text(),
     algorithm: str = settings.auth_jwt.algorithm,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
 ):
+    to_encode = payload.copy()
+    now = datetime.now(tz=timezone.utc)
+
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+
+    to_encode.update(
+        exp=expire,
+        iat=now,
+    )
     encoded = jwt.encode(
-        payload=payload,
+        payload=to_encode,
         key=private_key,
         algorithm=algorithm,
     )
